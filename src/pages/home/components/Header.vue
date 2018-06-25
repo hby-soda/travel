c08700b0ba62a549dc1f69490ce1b097424d3a5a
hby-soda <sodagreen_forever@qq.com>
2018-06-25 23:44:14 +0800

    city-router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -5,11 +5,14 @@
       </div>
       <div class="header-input">
         <span class="iconfont">&#xe632;</span>
-        输入城市/景点/游玩主题</div>
-      <div class="header-right">
-        {{city}}
-        <span class="iconfont arrow-icon">&#xe64a;</span>
+        输入城市/景点/游玩主题
       </div>
+      <router-link to="/city">
+        <div class="header-right">
+          {{city}}
+          <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
+      </router-link>
     </div>
 </template>
 
@@ -25,7 +28,7 @@ export default {
   @import '~styles/varibles.styl'
   .header
     display flex
-    line-height .86rem
+    line-height $headerHeight
     background-color $bgcolor
     color #ffffff
     .header-left
@@ -48,6 +51,7 @@ export default {
       width 1.24rem
       float right
       text-align center
+      color #fff
       .arrow-icon
         margin-left -.04rem
         font-size .24rem
